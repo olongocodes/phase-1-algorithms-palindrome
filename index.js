04250cab7720c208c834e62237fa3505efea50ef
Kevin <kevinolongo9@gmail.com>
2023-10-04 13:48:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
+function reverse(word){
+  // "abc" => 'cba'
+  const wordArray =word.split("")
+  const reversedWordArray = wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord;
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //reverse the input string
+  const reversedWord = reverse(word)
+  // if the input is the same as the reversed input
+  if (word === reversedWord){
+    //     return true
+    return true
+     // else
+  }else{
+    //   return false 
+    return false
+  }
+  
+
 }
 
 /* 
   Add your pseudocode here
+
+  that means the word is the same as the word in reverse, I shold return true
+
+  reverse the input string
+
+  if the input is the same as the reversed input
+      return true
+  else
+    return false    
+
 */
 
 /*
@@ -20,6 +52,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
